41bb9dd0d199037d3bd4cf3f75b00c8fa3f8d849
EZFelin <EFOliveira4@ucs.br>
2026-05-27 17:49:39 -0300

    Adicionada funções que mudam cor e fala se é positivo ou negativo

--- script.js
+++ script.js
@@ -3,6 +3,28 @@ const incrementBtn = document.getElementById('increment');
 const decrementBtn = document.getElementById('decrement');
 const resetBtn = document.getElementById('reset');
 const message = document.getElementById('message');
+function updateColor(count) {
+    if (count > 0) {
+        num.style.color = 'green';
+    }
+    if (count < 0) {
+        num.style.color = 'red';
+    }
+    if (count === 0) {
+        num.style.color = 'black';
+    }
+}
+function updateMessage(count) {
+    if (count > 0) {
+        message.textContent = "Contagem positiva!";
+    }
+    if (count < 0) {
+        message.textContent = "Contagem negativa!";
+    }
+    if (messageCount >= 100) {
+        message.textContent = "Você clicou mais de 100 vezes!";
+    }
+}
 let messageCount = 0;
 
 let count = 0;
@@ -10,30 +32,23 @@ incrementBtn.addEventListener('click', () => {
     count++;
     num.textContent = count;
     messageCount++;
-    if (count > 0) {
-        num.style.color = 'green';
-    }
-    if (count === 0) {
-        num.style.color = 'black';
-    }
+    updateColor(count);
+    updateMessage(count);
 });
 
 decrementBtn.addEventListener('click', () => {
     count--;
     num.textContent = count;
     messageCount++;
-    if (count < 0) {
-        num.style.color = 'red';
-    } 
-    if (count === 0) {
-        num.style.color = 'black';
-    }
+    updateColor(count);
+    updateMessage(count);
 });
 
 resetBtn.addEventListener('click', () => {
     count = 0;
     num.textContent = count;
-    num.style.color = 'black';
     message.textContent = messageCount;
     messageCount = 0;
+    updateColor(count);
+    updateMessage(count);
 })
